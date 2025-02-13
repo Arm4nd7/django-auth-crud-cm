51d26fb028879f666d7d28f77150da6e49996d12
Arm4nd7 <armando.estevezt@gmail.com>
2025-02-13 12:05:23 -0500

    actualizacion de base de datos v1.2.1

--- catalogo_muebles/settings.py
+++ catalogo_muebles/settings.py
@@ -80,25 +80,26 @@ WSGI_APPLICATION = 'catalogo_muebles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     #database de djang-render POSTGRES
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://postgres:123456789@localhost:5432/mueblesemae',
-#         conn_max_age=600,   
-#     )
-    
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mueblesemae',
-        'USER': 'postgres',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    #database de djang-render POSTGRES
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:123456789@localhost:5432/mueblesemae',
+        conn_max_age=600,   
+    )   
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mueblesemae',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456789',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
